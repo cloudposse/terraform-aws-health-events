8204f374ec51371e01b4327199e1262e429a878f
Giuliano Ursino <giuliano.ursino@nimbux911.com>
2024-06-24 11:53:13 -0300

    add sns_topic_arn output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "event_rule_arns" {
   description = "The ARNs of the created EventBridge Rules."
   value       = try([for event_rule in aws_cloudwatch_event_rule.health_events : event_rule.arn], [])
 }
+
+output "sns_topic_arn" {
+  description = "The ARN of the SNS topic"
+  value       = module.sns_topic.sns_topic_arn
+}
